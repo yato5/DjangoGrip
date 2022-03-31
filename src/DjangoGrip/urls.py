66c442c5842b8ded7f0535d92f739287b5bd40d8
yato5 <bouhouche.riy@gmail.com>
2022-03-31 18:32:19 +0200

    understand url

--- src/DjangoGrip/urls.py
+++ src/DjangoGrip/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.defaults import server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('505/', server_error),
 ]
